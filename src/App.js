b37a7951f0ca928274dea5ec22ad233590cedc9e
Joan Mira <JOAN.MIRA@VODAFONE.COM>
2019-02-07 17:19:47 +0100

    first push

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import ApolloClient, { gql } from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
+import logo from './logo.svg';
+import './App.css';
+
+const client = new ApolloClient({
+  uri: "/.netlify/functions/graphql"
+});
+
+const LambdaDemo = () => (
+  <ApolloProvider client={client}>
+    <Query
+      query={gql`
+        {
+          hello
+        }
+      `}
+    >
+      {({ data }) =>
+        <div>A greeting from the server: {data.hello}</div>}
+    </Query>
+  </ApolloProvider>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <LambdaDemo />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
